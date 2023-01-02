e947e6132f361de64ac2f946c447cb8753ffeff3
Ved <vedjain773@gmail.com>
2023-01-02 19:48:27 +0530

    added new methods and updated readme

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 # Usage
 
+### 1.  jumble()
+This method jumbles the characters of a string.
+
     const { jumble } = require("word-jumblr");
 
-    const exampleWord = "molarity";
-    const jumbledWord = jumble(exampleWord);
+    const word = "molarity";
+    console.log(jumble(word));
 
-    //possible output -> mloirayt
+    //possible output -> ioytmral
+    
+
+### 2. jumbleSentence()
+This method jumbles the words of a sentence or a statement.
+
+    const { jumbleSentence } = require("word-jumblr");
+
+    const sentence = "Programming is fun";
+    console.log(jumbleSentence(sentence));
+
+    //possible output -> fun programming is
+
+
+### 3. jumbleSentenceAdd()
+This method jumbles the words of the sentence and also jumbles the characters of each word.
+
+    const { jumbleSentenceAdd } = require("word-jumblr");
+
+    const sentence = "Programming is fun";
+    console.log(jumbleSentenceAdd(sentence));
+
+    //possible output -> unf si rogpmamginr
--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const jumble = (word) => {
         indexArr[i] = i;
     }
 
-    console.log(wordArr);
     let j = 0;
 
     while (wordArr.length != 0) {
-        let randomChar = wordArr[Math.floor(Math.random() * wordArr.length * 0.74)];
+        let randomChar = wordArr[Math.floor(Math.random() * wordArr.length)];
         let indexRemoved = wordArr.indexOf(randomChar);
         jumbledArr[j] = randomChar;
         wordArr.splice(indexRemoved, 1);
@@ -26,4 +25,56 @@ const jumble = (word) => {
     return jumbledArr.join("");
 }
 
+/**
+ * 
+ * @param {String} sentence 
+ */
+
+const jumbleSentence = (sentence) => {
+    if (!sentence.includes(" ")) {
+        return console.error("argument must contain whitespaces");
+    }
+
+    let wordArr = sentence.split(" ");
+    let indexArr = [];
+    let jumbledArr = [];
+
+    for (i = 0; i < wordArr.length; i++) {
+        indexArr[i] = i;
+    }
+
+    let j = 0;
+
+    while (wordArr.length != 0) {
+        let randomword = wordArr[Math.floor(Math.random() * wordArr.length)];
+        let indexRemoved = wordArr.indexOf(randomword);
+        jumbledArr[j] = randomword;
+        wordArr.splice(indexRemoved, 1);
+        j++;
+    }
+
+    return jumbledArr.join(" ");
+}
+
+/**
+ * 
+ * @param {String} sentence 
+ */
+
+const jumbleSentenceAdd = (sentence) => {
+    if (!sentence.includes(" ")) {
+        return console.error("argument must contain whitespaces");
+    }
+
+    const jumbledArr = jumbleSentence(sentence).split(" ");
+
+    jumbledArr.forEach((element, index, arr) => {
+        arr[index] = jumble(element);
+    });
+
+    return jumbledArr.join(" ");
+}
+
 module.exports.jumble = jumble;
+module.exports.jumbleSentence = jumbleSentence;
+module.exports.jumbleSentenceAdd = jumbleSentenceAdd;
